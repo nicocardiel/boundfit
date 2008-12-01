f9eed9c66855561e3d37d20f6226a49e0eadfa87
Nicolás Cardiel <nicocardiel@users.noreply.github.com>
2008-12-01 12:13:12 +0000

    Syncronizing the code of boundfit with xpgp

--- src/yfunk_pseudo.f
+++ src/yfunk_pseudo.f
@@ -1,24 +1,25 @@
 C------------------------------------------------------------------------------
 C Copyright 2008 Nicolas Cardiel
 C
-C This file is part of BoundFit.
+C This file is part of boundfit.
 C 
-C BoundFit is free software: you can redistribute it and/or modify
+C Boundfit is free software: you can redistribute it and/or modify
 C it under the terms of the GNU General Public License as published by
 C the Free Software Foundation, either version 3 of the License, or
 C (at your option) any later version.
 C 
-C BoundFit is distributed in the hope that it will be useful,
+C Boundfit is distributed in the hope that it will be useful,
 C but WITHOUT ANY WARRANTY; without even the implied warranty of
 C MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 C GNU General Public License for more details.
 C 
 C You should have received a copy of the GNU General Public License
-C along with BoundFit. If not, see <http://www.gnu.org/licenses/>.
+C along with boundfit. If not, see <http://www.gnu.org/licenses/>.
 C------------------------------------------------------------------------------
         REAL FUNCTION YFUNK_PSEUDO(X)
         IMPLICIT NONE
         INCLUDE 'ndatamax.inc'
+        INCLUDE 'nfixedmax.inc'
         INCLUDE 'ndegmax.inc'
 C
         REAL X(NDEGMAX+1)
@@ -28,10 +29,13 @@ C
         INTEGER J
         INTEGER NF,NTERMS
         INTEGER NDEG
+        INTEGER NFIXED
         REAL XF(NDATAMAX),YF(NDATAMAX),EYF(NDATAMAX)
+        REAL XFIXED(NFIXEDMAX),YFIXED(NFIXEDMAX)
+        REAL FIXEDWEIGHT
         REAL WEIGHT,POWER
-        REAL W1,W2
         REAL YPOL
+        DOUBLE PRECISION W1,W2
         DOUBLE PRECISION DSUM
         LOGICAL LUP
         REAL TSIGMA
@@ -40,6 +44,9 @@ C
         COMMON/BLKFUNKPSEUDO1/XF,YF,EYF
         COMMON/BLKFUNKPSEUDO2/WEIGHT,POWER,TSIGMA
         COMMON/BLKFUNKPSEUDO3/LUP
+        COMMON/BLKFIXED1/NFIXED
+        COMMON/BLKFIXED2/XFIXED,YFIXED
+        COMMON/BLKFIXED3/FIXEDWEIGHT
 C------------------------------------------------------------------------------
 C comprobacion inicial
         IF(TSIGMA.LT.0.0)THEN
@@ -54,49 +61,64 @@ C------------------------------------------------------------------------------
 C------------------------------------------------------------------------------
         IF(TSIGMA.EQ.0.0)THEN !.....................................sin errores
           IF(LUP)THEN
-            W1=1.0
-            W2=WEIGHT
+            W1=1.0D0
+            W2=DBLE(WEIGHT)
           ELSE
-            W1=WEIGHT
-            W2=1.0
+            W1=DBLE(WEIGHT)
+            W2=1.0D0
           END IF
           DO J=1,NF
             YPOL=FPOLY(NDEG,X,XF(J))
             IF(YPOL.GE.YF(J))THEN
-              DSUM=DSUM+W1*((YPOL-YF(J))**POWER)
+              DSUM=DSUM+W1*(DBLE(YPOL-YF(J))**DBLE(POWER))
             ELSE
-              DSUM=DSUM+W2*((YF(J)-YPOL)**POWER)
+              DSUM=DSUM+W2*(DBLE(YF(J)-YPOL)**DBLE(POWER))
             END IF
           END DO
+          IF(NFIXED.GT.0)THEN
+            DO J=1,NFIXED
+              YPOL=FPOLY(NDEG,X,XFIXED(J))
+              DSUM=DSUM+
+     +         DBLE(FIXEDWEIGHT)*DABS(DBLE(YFIXED(J)-YPOL))**DBLE(POWER)
+            END DO
+          END IF
         ELSE !......................................................con errores
           IF(LUP)THEN
             !aqui tenemos que usar ABS() porque podemos tener argumentos
             !negativos debido a que el IF() lo estamos calculando
             !considerando las barras de error
-            W1=1.0
-            W2=WEIGHT
+            W1=1.0D0
+            W2=DBLE(WEIGHT)
             DO J=1,NF
               YPOL=FPOLY(NDEG,X,XF(J))
               IF(YPOL.GE.YF(J)-TSIGMA*EYF(J))THEN !.......aqui usamos signo "-"
-                DSUM=DSUM+W1*(ABS(YPOL-YF(J))**POWER)
+                DSUM=DSUM+W1*(DABS(DBLE(YPOL-YF(J)))**DBLE(POWER))
               ELSE
-                DSUM=DSUM+W2*(ABS(YF(J)-YPOL)**POWER)
+                DSUM=DSUM+W2*(DABS(DBLE(YF(J)-YPOL))**DBLE(POWER))
               END IF
             END DO
           ELSE
-            W1=WEIGHT
-            W2=1.0
+            W1=DBLE(WEIGHT)
+            W2=1.0D0
             DO J=1,NF
               YPOL=FPOLY(NDEG,X,XF(J))
               IF(YPOL.GE.YF(J)+TSIGMA*EYF(J))THEN !.......aqui usamos signo "+"
-                DSUM=DSUM+W1*(ABS(YPOL-YF(J))**POWER)
+                DSUM=DSUM+W1*(DABS(DBLE(YPOL-YF(J)))**DBLE(POWER))
               ELSE
-                DSUM=DSUM+W2*(ABS(YF(J)-YPOL)**POWER)
+                DSUM=DSUM+W2*(DABS(DBLE(YF(J)-YPOL))**DBLE(POWER))
               END IF
+            END DO
+          END IF
+          IF(NFIXED.GT.0)THEN
+            DO J=1,NFIXED
+              YPOL=FPOLY(NDEG,X,XFIXED(J))
+              DSUM=DSUM+
+     +         DBLE(FIXEDWEIGHT)*DABS(DBLE(YFIXED(J)-YPOL))**DBLE(POWER)
             END DO
           END IF
         END IF
 C
+        DSUM=DSUM/DBLE(NF)
         YFUNK_PSEUDO=REAL(DSUM)
 C------------------------------------------------------------------------------
 100     FORMAT(A,$)
--- src/yfunk_splfit2.f
+++ src/yfunk_splfit2.f
@@ -1,25 +1,26 @@
 C------------------------------------------------------------------------------
 C Copyright 2008 Nicolas Cardiel
 C
-C This file is part of BoundFit.
+C This file is part of boundfit.
 C 
-C BoundFit is free software: you can redistribute it and/or modify
+C Boundfit is free software: you can redistribute it and/or modify
 C it under the terms of the GNU General Public License as published by
 C the Free Software Foundation, either version 3 of the License, or
 C (at your option) any later version.
 C 
-C BoundFit is distributed in the hope that it will be useful,
+C Boundfit is distributed in the hope that it will be useful,
 C but WITHOUT ANY WARRANTY; without even the implied warranty of
 C MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 C GNU General Public License for more details.
 C 
 C You should have received a copy of the GNU General Public License
-C along with BoundFit. If not, see <http://www.gnu.org/licenses/>.
+C along with boundfit. If not, see <http://www.gnu.org/licenses/>.
 C------------------------------------------------------------------------------
 C Funcion para minimizar la coordenada Y de un solo Knot
         REAL FUNCTION YFUNK_SPLFIT2(X)
         IMPLICIT NONE
         INCLUDE 'ndatamax.inc'
+        INCLUDE 'nfixedmax.inc'
         INCLUDE 'nknotsmax.inc'
         REAL X(NKNOTSMAX)
 C
@@ -27,12 +28,15 @@ C
         INTEGER I0
         INTEGER NF,ND
         INTEGER NREF
+        INTEGER NFIXED
         REAL XF(NDATAMAX),YF(NDATAMAX),EYF(NDATAMAX),YF0
+        REAL XFIXED(NFIXEDMAX),YFIXED(NFIXEDMAX)
+        REAL FIXEDWEIGHT
         REAL XDD(NKNOTSMAX)
         REAL S(NKNOTSMAX),A(NKNOTSMAX),B(NKNOTSMAX),C(NKNOTSMAX)
         REAL YD(NKNOTSMAX)
         REAL WEIGHT,POWER,TSIGMA
-        REAL W1,W2
+        DOUBLE PRECISION W1,W2
         DOUBLE PRECISION F
         LOGICAL LUP
 C
@@ -44,6 +48,9 @@ C
         COMMON/BLKSPLFUNK6/NREF
         COMMON/BLKSPLFUNK7/WEIGHT,POWER,TSIGMA
         COMMON/BLKSPLFUNK8/LUP
+        COMMON/BLKFIXED1/NFIXED
+        COMMON/BLKFIXED2/XFIXED,YFIXED
+        COMMON/BLKFIXED3/FIXEDWEIGHT
 C------------------------------------------------------------------------------
 C comprobacion inicial
         IF(TSIGMA.LT.0.0)THEN
@@ -58,20 +65,20 @@ C------------------------------------------------------------------------------
 C------------------------------------------------------------------------------
         IF(TSIGMA.EQ.0.0)THEN !.....................................sin errores
           IF(LUP)THEN
-            W1=1.0
-            W2=WEIGHT
+            W1=1.0D0
+            W2=DBLE(WEIGHT)
           ELSE
-            W1=WEIGHT
-            W2=1.0
+            W1=DBLE(WEIGHT)
+            W2=1.0D0
           END IF
           F=0.D0
           I0=1                   !la primera vez busca en el inicio de la tabla
           DO I=1,NF
             CALL CUBSPLX(XDD,YD,A,B,C,ND,I0,XF(I),YF0)
             IF(YF0.GE.YF(I))THEN
-              F=F+W1*((DBLE(YF0)-DBLE(YF(I)))**POWER)
+              F=F+W1*((DBLE(YF0)-DBLE(YF(I)))**DBLE(POWER))
             ELSE
-              F=F+W2*((DBLE(YF(I))-DBLE(YF0))**POWER)
+              F=F+W2*((DBLE(YF(I))-DBLE(YF0))**DBLE(POWER))
             END IF
           END DO
         ELSE !......................................................con errores
@@ -79,32 +86,40 @@ C------------------------------------------------------------------------------
             !aqui tenemos que usar ABS() porque podemos tener argumentos
             !negativos debido a que el IF() lo estamos calculando
             !considerando las barras de error
-            W1=1.0
-            W2=WEIGHT
+            W1=1.0D0
+            W2=DBLE(WEIGHT)
             F=0.D0
             I0=1                 !la primera vez busca en el inicio de la tabla
             DO I=1,NF
               CALL CUBSPLX(XDD,YD,A,B,C,ND,I0,XF(I),YF0)
               IF(YF0.GE.YF(I)-TSIGMA*EYF(I))THEN !........aqui usamos signo "-"
-                F=F+DBLE(W1)*(ABS(DBLE(YF0)-DBLE(YF(I)))**DBLE(POWER))
+                F=F+W1*(DABS(DBLE(YF0)-DBLE(YF(I)))**DBLE(POWER))
               ELSE
-                F=F+DBLE(W2)*(ABS(DBLE(YF(I))-DBLE(YF0))**DBLE(POWER))
+                F=F+W2*(DABS(DBLE(YF(I))-DBLE(YF0))**DBLE(POWER))
               END IF
             END DO
           ELSE
-            W1=WEIGHT
-            W2=1.0
+            W1=DBLE(WEIGHT)
+            W2=1.0D0
             F=0.D0
             I0=1                 !la primera vez busca en el inicio de la tabla
             DO I=1,NF
               CALL CUBSPLX(XDD,YD,A,B,C,ND,I0,XF(I),YF0)
               IF(YF0.GE.YF(I)+TSIGMA*EYF(I))THEN !........aqui usamos signo "+"
-                F=F+DBLE(W1)*(ABS(DBLE(YF0)-DBLE(YF(I)))**DBLE(POWER))
+                F=F+W1*(DABS(DBLE(YF0)-DBLE(YF(I)))**DBLE(POWER))
               ELSE
-                F=F+DBLE(W2)*(ABS(DBLE(YF(I))-DBLE(YF0))**DBLE(POWER))
+                F=F+W2*(DABS(DBLE(YF(I))-DBLE(YF0))**DBLE(POWER))
               END IF
             END DO
           END IF
+        END IF
+C------------------------------------------------------------------------------
+        I0=1
+        IF(NFIXED.GT.0)THEN
+          DO I=1,NFIXED
+            CALL CUBSPLX(XDD,YD,A,B,C,ND,I0,XFIXED(I),YF0)
+            F=F+DBLE(FIXEDWEIGHT)*DBLE(ABS(YFIXED(I)-YF0))**DBLE(POWER)
+          END DO
         END IF
 C------------------------------------------------------------------------------
         F=F/DBLE(NF)
